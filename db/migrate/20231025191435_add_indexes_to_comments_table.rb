2181d7ea9dd022fe97dd74821e95d208c9b034cb
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-25 16:40:07 -0300

    add indexes

--- db/migrate/20231025191435_add_indexes_to_comments_table.rb
+++ db/migrate/20231025191435_add_indexes_to_comments_table.rb
@@ -1,6 +1,6 @@
 class AddIndexesToCommentsTable < ActiveRecord::Migration[7.1]
   def change
-    add_index :likes, :user_id
-    add_index :likes, :post_id
+    add_index :comments, :user_id
+    add_index :comments, :post_id
   end
 end
--- db/migrate/20231025191511_add_indexes_to_posts_table.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexesToPostsTable < ActiveRecord::Migration[7.1]
-  def change
-    add_index :posts, :authors_id
-  end
-end
