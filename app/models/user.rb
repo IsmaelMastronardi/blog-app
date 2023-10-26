3e7e6c78ef1045a4fb9b9c4c08d47cf7bf7732ef
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-26 17:53:30 -0300

    add all_comments method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :posts, foreign_key: 'author_id'
   has_many :likes
+
+  def all_comments
+    comments.order(created_at: :asc).limit(3)
+  end
 end
