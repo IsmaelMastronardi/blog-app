c6599c55da0601e6876337e80376ed0ba65fffe9
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-27 12:09:26 -0300

    add test for post

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -23,4 +23,14 @@ RSpec.describe User, type: :model do
     expect(first_user.top_posts).to eq([fifth_post, fourth_post, third_post])
   end
 
+  it "gets the first 3 posts" do
+    first_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    first_post = Post.create(author: first_user, title: 'post 1', text: 'This is my first post')
+    second_post = Post.create(author: first_user, title: 'post 2', text: 'This is my second post')
+    third_post = Post.create(author: first_user, title: 'post 3', text: 'This is my third post')
+    fourth_post = Post.create(author: first_user, title: 'post 4', text: 'This is my fourth post')
+    fifth_post = Post.create(author: first_user, title: 'post 5', text: 'This is my fifth post')
+    expect(first_user.post_counter).to eq(5)
+  end
+
 end
