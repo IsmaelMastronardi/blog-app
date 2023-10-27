ba51d422592d42b27546e923d2205eb6f7cddf9f
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-27 13:18:12 -0300

    add post_counter validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :name, presence: true
+  validates :post_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   def top_posts
     posts.order(created_at: :desc).limit(3)
