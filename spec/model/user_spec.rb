8c738873c8a60f3f6339d6bf61e808c43f87cb3a
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-27 12:22:33 -0300

    removed usseless assigment to variable issues in the user test

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe User, type: :model do
 
   it 'gets the first 3 posts' do
     first_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-    first_post = Post.create(author: first_user, title: 'post 1', text: 'This is my first post')
-    second_post = Post.create(author: first_user, title: 'post 2', text: 'This is my second post')
+    Post.create(author: first_user, title: 'post 1', text: 'This is my first post')
+    Post.create(author: first_user, title: 'post 2', text: 'This is my second post')
     third_post = Post.create(author: first_user, title: 'post 3', text: 'This is my third post')
     fourth_post = Post.create(author: first_user, title: 'post 4', text: 'This is my fourth post')
     fifth_post = Post.create(author: first_user, title: 'post 5', text: 'This is my fifth post')
@@ -25,11 +25,11 @@ RSpec.describe User, type: :model do
 
   it 'gets the Users post_count' do
     first_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-    first_post = Post.create(author: first_user, title: 'post 1', text: 'This is my first post')
-    second_post = Post.create(author: first_user, title: 'post 2', text: 'This is my second post')
-    third_post = Post.create(author: first_user, title: 'post 3', text: 'This is my third post')
-    fourth_post = Post.create(author: first_user, title: 'post 4', text: 'This is my fourth post')
-    fifth_post = Post.create(author: first_user, title: 'post 5', text: 'This is my fifth post')
+    Post.create(author: first_user, title: 'post 1', text: 'This is my first post')
+    Post.create(author: first_user, title: 'post 2', text: 'This is my second post')
+    Post.create(author: first_user, title: 'post 3', text: 'This is my third post')
+    Post.create(author: first_user, title: 'post 4', text: 'This is my fourth post')
+    Post.create(author: first_user, title: 'post 5', text: 'This is my fifth post')
     expect(first_user.post_counter).to eq(5)
   end
 end
