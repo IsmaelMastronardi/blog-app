6766833c07c04ccbd88cbe1ffc171c6fcaa7d43e
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-11-03 16:13:36 -0300

    can see like added

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,11 @@
 class LikesController < ApplicationController
   def create
-    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
-    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
-    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
-    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
-    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
-    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
+    @poster = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
-    @new_like = Like.new(user_id: current_user, post_id: @post)
+    @new_like = Like.create(user: current_user, post: @post)
+    puts 'aaaaaaaaaaaaaaaaaaaaaaaaa'
     puts @new_like
+    redirect_to user_path(@poster)
   end
 
 end
