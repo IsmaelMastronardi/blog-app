37a16db2200265400190e3f71203d13978a37e6a
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-11-03 01:40:59 -0300

    add new, create and post_params methos to post controller - add new routes for osts, comments and likes - add post new.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,24 @@ class PostsController < ApplicationController
     @user = User.find(@post.author_id)
     @post_index = @user.posts.find_index { |post| post.id == @post.id }
   end
+
+  def new
+    @current_user = current_user
+    @new_post = Post.new
+  end
+
+  def create
+    puts 'aaaaaaaaaaaaaaa creating'
+    @new_post = current_user.posts.build(post_params)
+    puts @new_post.title
+    puts @new_post.text
+    puts 'aaaaaaaaaaaaaaa created'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
