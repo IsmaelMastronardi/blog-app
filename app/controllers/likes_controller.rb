9ce783347caa8f605b2b74fb36b635e2151bd9c2
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-11-03 16:03:04 -0300

    add like funcionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,14 @@
 class LikesController < ApplicationController
   def create
     puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
+    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
+    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
+    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
+    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
+    puts 'aaaaaaaaaaaaaaaaaaaaaaaaaa un like'
+    @post = Post.find(params[:post_id])
+    @new_like = Like.new(user_id: current_user, post_id: @post)
+    puts @new_like
   end
+
 end
