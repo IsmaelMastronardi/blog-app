b9759e1e2e5472f940727578ed0936d908b35542
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-11-06 22:39:45 -0300

    add correct fixtures to the user test

--- spec/features/user_index_integration_spec.rb
+++ spec/features/user_index_integration_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'User Index page', type: :system do
+  fixtures :users
+
   describe 'user index page' do
-    it 'shows the rigth content' do
+    it 'shows the right content' do
       visit root_path 
       sleep(5)
-      expect(page).to have_content('number of posts')
+      expect(page).to have_content('user 1')
     end
   end
 end
--- test/system/user_test.rb
+++ test/system/user_test.rb
@@ -1,9 +1,12 @@
 require 'application_system_test_case'
 
 class UsersTest < ApplicationSystemTestCase
+  before(:each) do
+    fixtures :users
+  end
   test 'my test' do
     visit root_path
     sleep(5)
-    assert_text "number of posts"
+    assert_text "user 1"
   end
 end
