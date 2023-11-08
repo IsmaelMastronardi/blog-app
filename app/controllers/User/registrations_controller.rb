96294e3d4d45364b82620b8c2a81afa5522aab7e
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-08 21:42:02 +0430

    Refactor sign_up_params method in User::RegistrationsController

--- app/controllers/User/registrations_controller.rb
+++ app/controllers/User/registrations_controller.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class User::RegistrationsController < Devise::RegistrationsController
-  private
-
-  def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+  
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -45,6 +41,11 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   # protected
 
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
