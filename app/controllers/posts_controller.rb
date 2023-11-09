2a77b8e6a5f7b00a0e32f551c34ebabe148ffbf2
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-11-09 14:15:05 -0300

    add post destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,25 @@ class PostsController < ApplicationController
     end
   end
 
+  # def destroy
+  #   post = Post.find(params[:id])
+  #   post.destroy
+  #   redirect_to user_posts_path(@user)
+  # end
+  def destroy
+    puts 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    @post = @user.posts.find(params[:id])
+    @post.likes.destroy_all
+    @post.comments.destroy_all
+    if @post.destroy
+      flash[:notice] = 'Post deleted!'
+      redirect_to user_posts_path(@user)
+    else
+      flash[:alert] = 'Failed to delete post!'
+      redirect_to user_post_path(@user)
+    end
+  end
+
   private
   
   def find_user
